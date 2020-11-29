acb011402ebe8614153f43fadd6d30471e76c67d
CptBeepy <73447630+CptBeepy@users.noreply.github.com>
2020-11-29 18:32:15 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package com.cis4615.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args){
+       	System.out.println("Use integer types that can fully represent the possible range of unsigned data");
+        getInteger();
+    }
+    
+    	public static int getInteger(){
+    	int is = 20;
+    	return is;
+	}
+}
